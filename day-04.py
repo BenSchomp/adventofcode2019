0d85b765ab2fff2e991e63ef28959cf902eed362
Ben Schomp <ben@benschomp.com>
2019-12-04 14:18:01 -0500

    combining part one and two; more efficient

--- day-04.py
+++ day-04.py
@@ -2,44 +2,32 @@ low = 359282
 high = 820401
 
 def is_valid( n ):
-  has_double = False
-  prev = int(n[0])
-  for i in n[1:]:
-    cur = int(i)
-    if cur < prev:
-      return False
-    elif cur == prev:
-      has_double = True
-
-    prev = cur
-    
-  return has_double
-
-def is_valid2( n ):
-  has_double = False
+  has_double1 = has_double2 = False
   matches = 0
   prev = int(n[0])
   for i in n[1:]:
     cur = int(i)
     if cur < prev:
-      return False
+      return (False, False)
     elif cur == prev:
+      has_double1 = True
       matches += 1
     else:
       if matches == 1:
-        has_double = True
+        has_double2 = True
       matches = 0
 
     prev = cur
 
-  return has_double or matches == 1
+  return (has_double1, has_double2 or matches == 1)
 
-count = count2 = 0
+count1 = count2 = 0
 for i in range(low, high+1):
   cur = str(i)
-  if is_valid(cur):
-    count += 1
-  if is_valid2(cur):
+  (valid1, valid2) = is_valid(cur)
+  if valid1:
+    count1 += 1
+  if valid2:
     count2 += 1
 
-print( count, count2 )
+print(count1, count2)
