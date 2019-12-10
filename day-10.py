a2fabd9dd175e865843b2f244dff7e73b2d0c1dd
Ben Schomp <ben@benschomp.com>
2019-12-10 15:16:33 -0500

    day 10; part one, now with radians!

--- day-10.py
+++ day-10.py
@@ -61,30 +61,8 @@ for location in asteroids:
     rise = asteroid[1]-location[1]
     run = asteroid[0]-location[0]
 
-    if run == 0:
-      if rise > 0:
-        slope = 'inf'
-      else:
-        slope = '-inf'
-    elif rise == 0:
-      if run > 0:
-        slope = '+0'
-      else:
-        slope = '-0'
-    else:
-      slope = Fraction( rise, run )
-      # objects along the same slope are blocked,
-      #  hence the tracking via the visible set
-      # however the location might between in the middle
-      #  of two objects on the same slope, so make each object's
-      #  slope "unique" by indicating if it is to the Left or Right
-      #  of vertical
-      if run < 0:
-        slope = str(slope)+'L'
-      else:
-        slope = str(slope)+'R'
-
-    visible.add(slope)
+    angle = atan2(rise, run)
+    visible.add(angle)
 
   if len(visible) > best:
     best = len(visible)
