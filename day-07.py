2f8d2c77a3289dbc8cee0c2bc8bc86b3227ead17
Ben Schomp <ben@benschomp.com>
2019-12-09 19:34:52 -0500

    part 2 done

--- day-07.py
+++ day-07.py
@@ -1,5 +1,6 @@
 my_input = [ 3,8,1001,8,10,8,105,1,0,0,21,38,63,72,81,106,187,268,349,430,99999,3,9,101,5,9,9,1002,9,3,9,101,3,9,9,4,9,99,3,9,102,3,9,9,101,4,9,9,1002,9,2,9,1001,9,2,9,1002,9,4,9,4,9,99,3,9,1001,9,3,9,4,9,99,3,9,102,5,9,9,4,9,99,3,9,102,4,9,9,1001,9,2,9,1002,9,5,9,1001,9,2,9,102,3,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99 ]
 example = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
+example2 = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
 
 ADD = 1
 MULTIPLY = 2
@@ -13,16 +14,16 @@ STOP = 99
 POSITION = '0'
 IMMEDIATE = '1'
 
-def process( data, the_input ):
-  i = 0
+def process( data, i, the_input ):
   output = None
   while( 1 ):
+    #print( data )
     cur = str(data[i]).zfill(5)
     opCode = int(cur[-2:])
     (mode1, mode2, mode3) = (cur[-3], cur[-4], cur[-5])
 
     if opCode == STOP:
-      break
+      return None
 
     if opCode in [ SET ]:
       p1 = int(data[i+1])
@@ -40,12 +41,11 @@ def process( data, the_input ):
       data[p3] = p1 * p2
       i += 4
     elif opCode == SET:
-      p1 = data[i+1]
       data[p1] = the_input.pop(0)
       i += 2
     elif opCode == GET:
-      output = p1
       i += 2
+      return (p1, i)
     elif opCode == JUMPT:
       if p1 != 0:
         i = p2
@@ -72,7 +72,6 @@ def process( data, the_input ):
       print( 'unknown opCode:', opCode )
       exit(-1)
 
-  return output
 
 def part_one( data ):
   max = 0
@@ -92,13 +91,14 @@ def part_one( data ):
 
             result = 0
             for phase in [a,b,c,d,e]:
-              result = process( data[:], [phase,result])
+              (result, x) = process( data[:], 0, [phase,result])
             if result > max:
               max = result
 
   return max
 
 def part_two( data ):
+
   max = 0
   for a in range(5):
     for b in range(5):
@@ -116,21 +116,32 @@ def part_two( data ):
 
             phases = [x+5 for x in [a,b,c,d,e]]
 
-            cur = 0
-            result = 0
+            amp_data = []
+            amp_input = []
+            result = []
+            pc = []
+            for i in range(5):
+              amp_data.append( data[:] )
+              result.append(0)
+              pc.append(0)
+              amp_input.append([phases[i]])
+            amp_input[0].append(0)
 
-            # keep looping around and around until all stop
-            # each amp needs its own, persisted copy of data array
-            # need some reworking here for part two ...
-            for phase in phases:
-              result = process( data[:], [phase,result])
-            if result > max:
-              max = result
+            cur_amp = 0
+            while( 1 ):
+              last_result = process( amp_data[cur_amp], pc[cur_amp], amp_input[cur_amp])
+              if last_result:
+                (result[cur_amp], pc[cur_amp]) = last_result
+              else:
+                break
+
+              cur_amp = (cur_amp + 1) % 5
+              amp_input[cur_amp].append(last_result[0])
+
+            if result[4] > max:
+              max = result[4]
 
   return max
 
 print( part_one( my_input ) )
-
-example = [ 3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26, 27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5 ]
-print( part_two( example ) )
-
+print( part_two( my_input ) )
