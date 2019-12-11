e2e72fe0f57233a20e480d9a8f96d20f99281489
Ben Schomp <ben@benschomp.com>
2019-12-11 12:54:00 -0500

    part two ... soooo close

--- day-10.py
+++ day-10.py
@@ -35,12 +35,50 @@ example1 = [
 '.##.#..###',
 '##...#..#.',
 '.#....####']
+example2 = [
+'.#....#####...#..',
+'##...##.#####..##',
+'##...#...#.#####.',
+'..#.....#...###..',
+'..#.#.....#....##']
+example3 = [
+'.#..##.###...#######',
+'##.############..##.',
+'.#.######.########.#',
+'.###.#######.####.#.',
+'#####.##.#.##.###.##',
+'..#####..#.#########',
+'####################',
+'#.####....###.#.#.##',
+'##.#################',
+'#####.##.###..####..',
+'..######..##.#######',
+'####.##.####...##..#',
+'.#####..#.######.###',
+'##...#.##########...',
+'#.##########.#######',
+'.####.#.###.###.#.##',
+'....##.##.###..#####',
+'.#.#.###########.###',
+'#.#.#.#####.####.###',
+'###.##.####.##.#..##']
 
-from fractions import Fraction
+from math import atan2, sqrt, pi
 
-asteroids = set()
+def getAngle( source, dest ):
+  rise = source[1]-dest[1]
+  run = source[0]-dest[0]
+
+  angle = atan2(rise, run)
+  return angle
+
+def getDist( source, dest ):
+  return sqrt((dest[0]-source[0])**2 + (dest[1]-source[1])**2)
+
+
+asteroids = set() 
 y = 0
-for line in my_input:
+for line in example3:
   x = 0
   for digit in line:
     if digit == '#':
@@ -51,6 +89,7 @@ for line in my_input:
 height = y
 width = x
 
+# part_one
 best = 0
 for location in asteroids:
   visible = set()
@@ -58,14 +97,60 @@ for location in asteroids:
     if location == asteroid:
       continue
 
-    rise = asteroid[1]-location[1]
-    run = asteroid[0]-location[0]
-
-    angle = atan2(rise, run)
-    visible.add(angle)
+    visible.add( getAngle( location, asteroid ) )
 
   if len(visible) > best:
     best = len(visible)
     best_location = location
 
-print( best, best_location )
+print( 'part_one:', best, best_location )
+
+# part_two
+# calculate angle and distance from laser location to each asteroid
+targets = {}
+angles = set()
+for asteroid in asteroids:
+  x = asteroid[0]
+  y = asteroid[1]
+  angle = getAngle( best_location, asteroid )
+  distance = getDist( best_location, asteroid )
+  node = (distance, (x,y))
+  if angle not in targets:
+    targets[angle] = [node]
+  else:
+    targets[angle].append( node )
+    targets[angle].sort()
+
+  angles.add( angle )
+
+angles = sorted(angles)
+
+# start laser straight up (ie pi/2)
+a = 0
+a_max = len(angles)-1
+while a <= a_max and angles[a] < (pi/2):
+  a += 1
+a %= a_max
+
+# laser is set ... start shootin' shit
+print( 'part_two' )
+count = miss = 0
+while len(targets) > 0:
+  cur_angle = angles[a]
+
+  if cur_angle in targets:
+    count += 1
+    casualty = targets[cur_angle].pop(0)
+    print( count, casualty[1], cur_angle, casualty[0] )
+
+    if len(targets[cur_angle]) < 1:
+      del targets[cur_angle]
+  else:
+    print('.', end='')
+    miss += 1
+    if miss > 20:
+      print( angles )
+      print( targets )
+      exit()
+
+  a = (a + 1) % a_max
